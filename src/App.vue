1df855e23bc9e850da861982f96c8258c4ec4130
Diogo Alhada <93165102+Alhada06@users.noreply.github.com>
2022-12-09 23:03:16 +0000

    solution implemented , tests passed on comented solutions and added extra solution with a useCicleList and ascending and descending sorting

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,32 @@
+
+<script setup>
+import presents from './presents.json'
+import { ref, computed } from 'vue';
+import { useCycleList } from '@vueuse/core'
+
+// const activeSortMode = ref('default');
+
+const sortModes = {
+  default: (a, b) => a.id - b.id,
+  asc: (a, b) => a.dimensions.width * a.dimensions.height - b.dimensions.height * b.dimensions.width,
+  desc: (a, b) => b.dimensions.width * b.dimensions.height - a.dimensions.width * a.dimensions.height,
+}
+// const toggleSort = () => {
+//   activeSortMode.value = activeSortMode.value === 'default' ? 'asc' : 'default';
+//   console.log('toggled', activeSortMode.value)
+// }
+const { state, next } = useCycleList([
+  'default', 'asc', 'desc'
+])
+
+// const sortedPresents = computed(() => [...presents].sort(sortModes[activeSortMode.value]))
+
+const sortedPresents = computed(() => [...presents].sort(sortModes[state.value]))
+
+</script>
+
+
+
 <template>
   <div class="w-full h-full p-4 flex justify-center items-center">
     <div class="max-w-md">
@@ -5,18 +34,12 @@
         <img src="/assets/tree.svg" alt="Christmas tree" />
       </div>
       <div class="mt-2 flex justify-center items-center">
-        <img
-          v-for="present in presents"
-          :key="present.id"
-          :src="present.src"
-          :alt="`Present ${present.id}`"
-          data-qa="present"
-        />
+        <img v-for="present in sortedPresents" :key="present.id" :src="present.src" :alt="`Present ${present.id}`"
+          data-qa="present" />
       </div>
+      <button @click="next()"
+        class=" bg-red-600 px-6 py-2 rounded-lg flex items-center justify-center mx-auto mt-8 hover:bg-red-700 text-white">Toggle
+        sort</button>
     </div>
   </div>
 </template>
-
-<script setup>
-import presents from './presents.json'
-</script>
